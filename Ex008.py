9201ca5c6f4ba9a13798d1879ae1f60a3823dda2
Davi Oliveira <rdavioliveira2020@gmail.com>
2025-01-13 21:42:14 -0300

    Exercício 008 do Curso de Python do Gustavo Guanabara

---
+++ Ex008.py
@@ -0,0 +1,8 @@
+'''from math import trunc
+num = float(input("Digite um número: "))
+print(f"O número digitado foi {num} \n Sua forma inteira: {trunc(num)}")'''
+
+#OU
+
+num = float(input("Digite o número: "))
+print(f"O número é {num}, sua forma inteira é: {int(num)}")
