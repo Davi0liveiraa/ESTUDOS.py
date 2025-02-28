7ea3cd17d4285291953eb7d5603afdd7fc495712
Davi0liveiraa <rdavioliveira2020@gmail.com>
2025-02-28 20:54:23 -0300

    Ajuste

--- Ex029.py
+++ Ex029.py
@@ -1,9 +1,8 @@
 '''Escreva um programa que leia a velocidade de um carro. Se ele ultrapassar 80Km/h, mostre uma mensagem dizendo que ele foi multado.
 A multa vai custar R$7,00 por cada Km acima do limite.'''
 
-velocidade = int(input("Digite a velocidade do carro: "))
+velocidade = int(input("Digite a velocidade atual do carro: "))
 if velocidade > 80:
     multa = 7*(velocidade-80)
     print(f"Você foi multado! Valor: R${multa:.2f}")
-else:
-    print("Você está limpo.")
+print("Você está limpo.")
