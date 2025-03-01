20f7de52550db996d952a716dac531666791c6b3
Davi0liveiraa <rdavioliveira2020@gmail.com>
2025-02-28 21:16:10 -0300

    Ajuste - Comando para identificar o ano atual e análisar se é bissexto.

--- Ex032.py
+++ Ex032.py
@@ -1,11 +1,13 @@
 '''Faça um programa que leia um ano qualquer e mostre se ele é bissexto.'''
 
-ano = int(input("Digite um ano qualquer: "))
-dias = int(input("Digite a quantidade de dias desse ano: "))
+from datetime import date
+ano = int(input("Digite um ano para análisar, ou digite [0] para análisar o ano atual."))
+if ano == 0:
+    ano = date.today().year 
+if ano % 4 == 0 and ano % 100 != 0  or ano % 400 == 0:
+    print(f"O ano de {ano} é BISSEXTO!")
+else:
+    print(f"O ano de {ano} não é bissexto!")
 
-if dias != 365 and dias != 366:
-    print("Ano inválido.\n") 
-elif dias == 365:
-    print("Ano normal.\n")
-elif dias == 366:
-    print("Ano bissexto!\n")
+
+ 
